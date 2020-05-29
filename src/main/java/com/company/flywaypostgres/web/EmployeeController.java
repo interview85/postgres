aed296fa384a466f7486a594e5c3e4d0ff3a5e48
interview85 <andreas.gelever@gmail.com>
2020-05-29 14:45:18 +0300

    web added, empty controller

---
+++ src/main/java/com/company/flywaypostgres/web/EmployeeController.java
@@ -0,0 +1,10 @@
+package com.company.flywaypostgres.web;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class EmployeeController {
+
+}
